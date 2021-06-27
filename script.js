8d0cabe15942117c08d00bd7d472f87ed3627269
Shoaib Amin <shobi@ymail.com>
2021-06-27 11:42:58 +0500

    modified the three files to complete the simple interest calculator project

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    rate =document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    interest = p * years * rate /100;
+    year = new Date().getFullYear() + parseInt(years);    
+       
+    if (p < 1) {
+    	alert ("Please input a positive number");
+    	return false;
+    }   
+
+    else
+    	{
+    		document.getElementById("result").innerHTML="If you deposit " +p+  ",\<br\>at an interest rate of "+rate+" %\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    		return false;
+    	}
 }
-        
+
+function updateRate()
+{
+    rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
+function validate()
+{
+
+}
